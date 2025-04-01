0ce42fa4a805214c3e6079ad981b4c4a20066cd2
VoltedSwitch <wirelessvoltswitch@gmail.com>
2025-04-01 15:20:22 +0500

    Modify: ChecklistsManager class
    Add: reposition_checklist method
    Improve: remove_checklist method

--- checklist_core/checklist_manager.py
+++ checklist_core/checklist_manager.py
@@ -7,15 +7,21 @@ class ChecklistsManager:
     def add_checklist(self, checklist: Checklist) -> None:
         self.user_checklists.append(checklist)
 
-    def insert_checklist(self, checklist: Checklist, checklist_number: int) -> None:
+    def insert_checklist(self, checklist_number: int, checklist: Checklist,) -> None:
         self.user_checklists.insert(checklist_number - 1, checklist)
 
     def rename_checklist(self, checklist_number: int, checklist_new_name: str) -> None:
         self.get_checklist(checklist_number).name = checklist_new_name
 
-    def remove_checklist(self, checklist_number: int) -> None:
-        self.user_checklists.pop(checklist_number -  1)
+    def reposition_checklist(self, checklist_number: int, insertion_position: int) -> None:
+        checklist: Checklist = self.remove_checklist(checklist_number, return_removed_checklist=True)
+        self.insert_checklist(insertion_position, checklist)
 
+    def remove_checklist(self, checklist_number: int, return_removed_checklist: bool=False) -> None | Checklist:
+        if return_removed_checklist:
+            return self.user_checklists.pop(checklist_number -  1)
+        self.user_checklists.pop(checklist_number -  1)
+        
     def clear_all_checklists(self) -> None:
         self.user_checklists.clear()
 
